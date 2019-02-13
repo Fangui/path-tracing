09e3b6a30b06c274f4ee2d6fc17f945244e5c094
Clément Fang <clement.fang@epita.fr>
2019-02-13 19:03:45 +0100

    Add basic vector operations

---
+++ src/vector.cc
@@ -0,0 +1,77 @@
+#include "vector.hh"
+
+Vector Vector::operator+(const Vector &rhs) const
+{
+    return Vector(x_, y_, z_ ) += rhs;
+}
+
+Vector Vector::operator+=(const Vector &rhs)
+{
+    x_ += rhs.x_;
+    y_ += rhs.y_;
+    z_ += rhs.z_;
+
+    return *this;
+}
+
+Vector Vector::operator*(int lambda) const
+{
+    return Vector(x_, y_, z_ ) *= lambda;
+}
+
+Vector Vector::operator*=(int lambda)
+{
+    x_ *= lambda;
+    y_ *= lambda;
+    z_ *= lambda;
+
+    return *this;
+}
+
+Vector Vector::operator*(const Vector &rhs) const
+{
+    return Vector(x_ * rhs.x_, y_ * rhs.y_, z_ * rhs.z_);
+}
+
+Vector Vector::operator*=(const Vector &rhs)
+{
+    x_ *= rhs.x_;
+    y_ *= rhs.y_;
+    z_ *= rhs.z_;
+
+    return *this;
+}
+
+Vector Vector::cross_product(const Vector &rhs) const
+{
+    return Vector(x_, y_, z_).cross_product_inplace(rhs);
+}
+
+Vector Vector::cross_product_inplace(const Vector &rhs)
+{
+    float x = y_ * rhs.z_ - z_ * rhs.y_;
+    float y = z_ * rhs.x_ - x_ * rhs.z_;
+    float z = x_ * rhs.y_ - y_ * rhs.x_;
+
+    x_ = x;
+    y_ = y;
+    z_ = z;
+
+    return *this;
+}
+
+Vector Vector::norm(void) const
+{
+    return Vector(x_, y_, z_).norm_inplace();
+}
+
+Vector Vector::norm_inplace(void)
+{
+    float dist = this->get_dist();
+
+    x_ /= dist;
+    y_ /= dist;
+    z_ /= dist;
+
+    return *this;
+}
---
+++ src/vector.hh
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cmath>
+
+class Vector
+{
+public:
+    Vector(float x, float y, float z) : x_(x), y_(y), z_(z) {};
+    Vector operator+(const Vector &rhs) const;
+    Vector operator+=(const Vector &rhs);
+    Vector operator*(int lambda) const;
+    Vector operator*=(int lambda);
+    Vector operator*(const Vector &rhs) const;
+    Vector operator*=(const Vector &rhs);
+    Vector cross_product(const Vector &rhs) const;
+    Vector cross_product_inplace(const Vector &rhs);
+    Vector norm(void) const;
+    Vector norm_inplace(void);
+
+private:
+    inline float get_dist() { return sqrtf(x_ * x_ + y_ * y_ + z_ * z_); };
+    float x_;
+    float y_;
+    float z_;
+};
