5052468ff2bc0c9a7ff66828e21720b3d14cc398
Clément Fang <clement.fang@epita.fr>
2019-02-24 00:01:14 +0100

    Clean code + small opti in parse obj

--- src/parse.cc
+++ src/parse.cc
@@ -60,32 +60,24 @@ std::vector<Triangle> obj_to_vertices(const std::string &s)
     std::string line;
 
     float val[3];
-    unsigned cpt = 0;
     while (std::getline(in, line))
     {
-        cpt = 0;
         if (line[0] == 'v') // vertices
         {
+            unsigned cpt = 0;
             for (unsigned i = 2; i < line.size(); ++i)
             {
-                std::string s;
-                s.reserve(line.size());
-
                 if (is_blank(line[i]))
                     continue;
 
+                std::string s;
+                s.reserve(line.size() - i);
                 while (i < line.size() && !is_blank(line[i]))
-                {
-                    s += line[i];
-                    ++i;
-                }
+                    s += line[i++];
 
                 val[cpt++] = stof(s);
             }
-        }
 
-        if (cpt == 3)
-        {
             Vector vect(val[0], val[1], val[2]);
             if (line[1] == 'n') // vn
                 vn.push_back(vect);
@@ -95,11 +87,10 @@ std::vector<Triangle> obj_to_vertices(const std::string &s)
     }
 
     vt.reserve(v.size() / 3);
-    for (unsigned i = 0; i < v.size(); i += 3)
+    for (unsigned i = 0; i < v.size() / 3; ++i)
     {
-        unsigned idx = i / 3;
-        Triangle t(v[idx], v[idx + 1], v[idx + 2],
-                   vn[idx], vn[idx + 1], vn[idx + 2]);
+        Triangle t(v[i], v[i + 1], v[i + 2],
+                   vn[i], vn[i + 1], vn[i + 2]);
 
         vt.push_back(t);
     }
